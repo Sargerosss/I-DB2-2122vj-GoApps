04f335d209d0e8170a411c326b52591323c8949b
Martijn <martijn.koreman@student.fontys.nl>
2022-06-16 09:47:12 +0200

    Small fix

--- Challenge/Code/submit-helpdesk.go
+++ Challenge/Code/submit-helpdesk.go
@@ -14,8 +14,8 @@ func submitHelpdesk(user User, db *sql.DB) {
 	scanner.Scan()
 	problem := scanner.Text()
 	fmt.Println("How can we contact you? In case of a serious problem, we would like to keep you updated on our progress")
-	fmt.Println("You agree by receiving emails about your request, and you can always ask us to delete your data.")
-	fmt.Println("If you don't agree, please let us know it's not a real address/leave it blank/put in no")
+	fmt.Println("You agree by receiving emails/phone calls about your request, and you can always ask us to delete your personal data.")
+	fmt.Println("If you don't agree, please let us know it's not a real email address/leave it blank/put in no/not a real phone number")
 	fmt.Print("Your contact information: ")
 	var contact string
 	fmt.Scanln(&contact)
@@ -25,8 +25,8 @@ func submitHelpdesk(user User, db *sql.DB) {
 	helpdeskRequest(user, db, problem, contact)
 	time.Sleep(2 * time.Second)
 	fmt.Println("-----------------------")
-	fmt.Println("Result")
 	fmt.Println("Sent the request")
+	fmt.Println("Sending you back now")
 	time.Sleep(1 * time.Second)
 	extendedToolSelect(user, db)
 }
